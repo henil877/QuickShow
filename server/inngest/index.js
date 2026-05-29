e1c441f8d457c0ef2b29936a4cad4571d06b346b
henil877 <henildarji94@gmail.com>
2026-05-29 13:54:55 +0530

    commit

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -6,7 +6,7 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 const syncUserCreation = inngest.createFunction(
   {
     id: "sync-user-from-clerk",
-    trigger: { event: "clerk/user.created" },
+    triggers: { event: "clerk/user.created" },
   },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
@@ -14,7 +14,7 @@ const syncUserCreation = inngest.createFunction(
     const userData = {
       _id: id,
       email: email_addresses?.[0]?.email_address || "",
-      name: `${first_name || ""} ${last_name || ""}`.trim(),
+      name: `${first_name || ""} ${last_name || ""}`.trim() || "No Name",
       image: image_url || "",
     };
 
@@ -30,7 +30,7 @@ const syncUserCreation = inngest.createFunction(
 const syncUserDeletion = inngest.createFunction(
   {
     id: "delete-user-with-clerk",
-    trigger: { event: "clerk/user.deleted" },
+    triggers: { event: "clerk/user.deleted" },
   },
   async ({ event }) => {
     const { id } = event.data;
@@ -44,7 +44,7 @@ const syncUserDeletion = inngest.createFunction(
 const syncUserUpdation = inngest.createFunction(
   {
     id: "update-user-from-clerk",
-    trigger: { event: "clerk/user.updated" },
+    triggers: { event: "clerk/user.updated" },
   },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
@@ -52,7 +52,7 @@ const syncUserUpdation = inngest.createFunction(
     const userData = {
       _id: id,
       email: email_addresses?.[0]?.email_address || "",
-      name: `${first_name || ""} ${last_name || ""}`.trim(),
+      name: `${first_name || ""} ${last_name || ""}`.trim() || "No Name",
       image: image_url || "",
     };
 
