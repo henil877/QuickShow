8720b8b7e9bf63861aa5b82f45c1c5b8c8949d05
henil877 <henildarji94@gmail.com>
2026-06-07 05:57:17 +0530

    function added to release seats after 10 minutes

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import Booking from "../models/Booking.js";
+import Show from "../models/Show.js";
 
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
@@ -63,8 +65,45 @@ const syncUserUpdation = inngest.createFunction(
   }
 );
 
+const releaseSeatsAndDeleteBooking = inngest.createFunction(
+  {
+    id: "release-seats-delete-booking",
+    triggers: [{ event: "app/checkpayment" }],
+  },
+  async ({ event, step }) => {
+    const tenMinutesLater = new Date(Date.now() + 10 * 60 * 1000);
+
+    await step.sleepUntil("wait-for-10-minutes", tenMinutesLater);
+
+    await step.run("check-payment-status", async () => {
+      const bookingId = event.data.bookingId;
+      const booking = await Booking.findById(bookingId);
+
+      if (!booking) return;
+
+      if (!booking.isPaid) {
+        const show = await Show.findById(booking.show);
+
+        if (!show) return;
+
+        booking.bookedSeats.forEach((seat) => {
+          delete show.occupiedSeats[seat];
+        });
+
+        show.markModified("occupiedSeats");
+        await show.save();
+
+        await Booking.findByIdAndDelete(booking._id);
+      }
+    });
+
+    return { success: true };
+  }
+);
+
 export const functions = [
   syncUserCreation,
   syncUserDeletion,
   syncUserUpdation,
+  releaseSeatsAndDeleteBooking,
 ];
